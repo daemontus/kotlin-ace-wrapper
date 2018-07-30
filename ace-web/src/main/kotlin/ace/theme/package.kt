dbb0adcedc72e9fc98d594cc0b18d81264d9aa00
Samuel Pastva <daemontus@gmail.com>
2018-07-30 13:24:42 +0200

    bugfixes

--- ace-web/src/main/kotlin/ace/theme/package.kt
+++ ace-web/src/main/kotlin/ace/theme/package.kt
@@ -1,13 +1,13 @@
 package ace.theme
 
-import ace.ACE
+import ace.Ace
 import ace.lib.DOM
 
 /**
- * Register a [Theme] object object using the global [ACE] instance.
+ * Register a [Theme] object object using the global [Ace] instance.
  */
 fun Theme.register() {
-    ACE.define(id, arrayOf("require", "exports")) { _: dynamic, module: dynamic ->
+    Ace.define(id, arrayOf("require", "exports")) { _: dynamic, module: dynamic ->
         module.isDark = isDark
         module.cssClass = cssClass
         module.cssText = cssString
